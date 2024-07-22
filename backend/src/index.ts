0eea777f195f54820e4f8ea1bf5b23ecbc725bf9
Abhijeet Hiwale <abhijeet.hiwale@paycraftsol.com>
2024-07-22 13:53:34 +0530

    Verify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
-// url : postgresql://myDB_owner:RaPEZQJ42lkY@ep-tight-river-a11txxui.ap-southeast-1.aws.neon.tech/myDB?sslmode=require
 import { Express } from "express";
+
+
+
 const express = require('express')
 const app = express();
 const port = 3000
